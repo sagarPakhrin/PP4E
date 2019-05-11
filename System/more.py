e1a00a263cd80eab4927fbe253941da0e498220a
Sagar Lama <sagarlama826@gmail.com>
2019-05-11 12:52:34 +0545

    System

---
+++ System/more.py
@@ -0,0 +1,15 @@
+"""
+Split and interactively page a string of file of text
+"""
+
+def more(text,numlines=1):
+    lines = text.splitlines()
+    while lines:
+        chunk = lines[:numlines]
+        lines = lines[numlines:]
+        for line in chunk:print(line)
+        if lines and input('More?') not in ['y','Y']:break
+
+if __name__ == "__main__":
+    import sys
+    more(open(sys.argv[1]).read(),10)
