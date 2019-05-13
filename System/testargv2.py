f0a8ab4f98788683289e7a70dfb9963d7b1b8ccb
sagar <sagarlama826@gmail.com>
2019-05-13 07:23:56 +0545

    gui

---
+++ System/testargv2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+def getopts(argv):
+    opts = {}
+    while argv:
+        if argv[0] == '-':
+            opts[argv[0]] = argv[1]
+            argv = argv[2:]
+        else:
+            argv = argv[1:]
+    return opts
+
+if __name__ == "__main__":
+    from sys import argv
+    myargs = getopts(argv)
+    if '-' in myargs:
+        print(myargs['-i'])
+    print(myargs)
