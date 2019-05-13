1bac9b56d6172593233cda628444f1bae15aacd7
sagar <sagarlama826@gmail.com>
2019-05-13 07:23:40 +0545

    Gui entr section

---
+++ Gui/Intro/gui1.py
@@ -0,0 +1,4 @@
+from tkinter import Label
+widget = Label(None, text='Hello GUI world!')   #get a widget object
+widget.pack()                                   # make one
+widget.mainloop()                               # start event loop
